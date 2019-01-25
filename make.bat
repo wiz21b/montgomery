b9f229b1227f73acd042372de66422f014e452ef
Stefan Champailler <stephane.champailler@smals.be>
2019-01-25 10:43:59 +0100

    progress

--- make.bat
+++ make.bat
@@ -4,4 +4,4 @@ python setup-wheel.py clean bdist_wheel
 rmdir /q /s build
 rmdir /q /s pyxfer.egg-info
 
-REM pip install --upgrade --no-deps --force-reinstall dist\pyxfer-0.1.0-py3-none-any.whl
+pip install --upgrade --no-deps --force-reinstall dist\pyxfer-0.1.0-py3-none-any.whl
